c8c5d4d0f8de263ff7aa60149ef45b5c53373929
TamaraKoval <kovalpsy@mail.ru>
2023-03-23 11:50:54 +0300

    Tests init

---
+++ src/test/java/Analyzer/PurchaseAnalyzerTest.java
@@ -0,0 +1,20 @@
+package Analyzer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchaseAnalyzerTest {
+
+    @Test
+    void prepareDataForAnalysis() {
+    }
+
+    @Test
+    void doAnalytics() {
+    }
+
+    @Test
+    void getReport() {
+    }
+}
